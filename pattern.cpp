0e86fcfecc900180efb10ed39b5d9a58829ad297
nitesh patel <nitesh2492874@gmail.com>
2025-10-26 16:12:27 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n=4;
+    int num =4; 
+    for(int i=0; i<n; i++){
+        for(int j=0; j<num; j++){
+        cout<< char('A' + j) <<" ";
+        }
+        cout<<endl;
+    }
+return 0;
+}
