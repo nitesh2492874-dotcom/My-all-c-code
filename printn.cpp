36979c80bc52bf4a23a9e159c92056e0e3e10b43
nitesh patel <nitesh2492874@gmail.com>
2025-10-26 16:13:26 +0530

    Add files via upload

---
+++ printn.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void printnums(int n){
+    if(n==1){
+        cout<<"1\n";
+        return;
+    }
+    cout<<n<<" ";
+    printnums(n-1);
+}
+int main(){
+    printnums(4);
+    return 0;
+}
